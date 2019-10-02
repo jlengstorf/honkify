9a31201e16674a295b670f4c233e2b3af7cf76f8
David Li <david.li3100@gmail.com>
2019-10-02 10:46:30 -0400

    fix indenting

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ const honkify = () => {
   }
 
    //check if the user is on iOS Safari, if they are, we initiate the safari audio "hack"
-   const ua = window.navigator.userAgent;
-   const iOS = !!ua.match(/iPad/i) || !!ua.match(/iPhone/i);
-   const webkit = !!ua.match(/WebKit/i);
-   const iOSSafari = iOS && webkit && !ua.match(/CriOS/i);
+  const ua = window.navigator.userAgent;
+  const iOS = !!ua.match(/iPad/i) || !!ua.match(/iPhone/i);
+  const webkit = !!ua.match(/WebKit/i);
+  const iOSSafari = iOS && webkit && !ua.match(/CriOS/i);
 
   const audio = new Audio(
     'https://res.cloudinary.com/jlengstorf/video/upload/q_auto/v1569957993/honk-sound.mp3',
