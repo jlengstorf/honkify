2f98617065aef69a99f231ac4883910d3e0fa60c
David Li <david.li3100@gmail.com>
2019-10-02 10:31:52 -0400

    Honk for iOS Safari

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const honkify = () => {
+
+  //check if the user is on iOS Safari, if they are, we initiate the safari audio "hack"
+  var ua = window.navigator.userAgent;
+  var iOS = !!ua.match(/iPad/i) || !!ua.match(/iPhone/i);
+  var webkit = !!ua.match(/WebKit/i);
+  var iOSSafari = iOS && webkit && !ua.match(/CriOS/i);
+
   if (typeof window === 'undefined') {
     console.warn('honkify only works in the browser.');
     console.warn('I mean... honk!');
@@ -8,6 +15,20 @@ const honkify = () => {
   const audio = new Audio(
     'https://res.cloudinary.com/jlengstorf/video/upload/q_auto/v1569957993/honk-sound.mp3',
   );
+
+  /**
+   * if the platform is iOS Safari, we quickly play, pause, reset the audio object
+   * this allows it to be playable the next time an user clicks a link
+   */
+
+  if (iOSSafari) {
+    console.log('Honk! This is safari')
+    document.body.addEventListener('touchstart', () => {
+      audio.play();
+      audio.pause();
+      audio.currentTime = 0;
+    })
+  }
 
   const links = document.querySelectorAll('a');
 
