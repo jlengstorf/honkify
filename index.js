8cda69d36b3fb06e0c52de441e16598717cb236f
Ryan Stelly <ryan.stelly@live.com>
2019-10-02 10:48:20 -0500

    SSR friendliness
    Co-Authored-By: Jason Lengstorf <jason@lengstorf.com>

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const honkify = (elementToHonkify = document) => {
+const honkify = (elementToHonkify = false) => {
   if (typeof window === 'undefined') {
     console.warn('honkify only works in the browser.');
     console.warn('I mean... honk!');
@@ -29,7 +29,7 @@ const honkify = (elementToHonkify = document) => {
     })
   }
 
-  const links = elementToHonkify.querySelectorAll('a');
+  const links = (elementToHonkify || document).querySelectorAll('a');
 
   const honk = event => {
     event.preventDefault();
