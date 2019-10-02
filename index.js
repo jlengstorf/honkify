b9c0c13e9e473135f93a92b34ca34112162ecc9a
David Li <david.li3100@gmail.com>
2019-10-02 10:45:31 -0400

    ES6 things, avoid SSR issues

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const honkify = () => {
 
-  //check if the user is on iOS Safari, if they are, we initiate the safari audio "hack"
-  var ua = window.navigator.userAgent;
-  var iOS = !!ua.match(/iPad/i) || !!ua.match(/iPhone/i);
-  var webkit = !!ua.match(/WebKit/i);
-  var iOSSafari = iOS && webkit && !ua.match(/CriOS/i);
-
   if (typeof window === 'undefined') {
     console.warn('honkify only works in the browser.');
     console.warn('I mean... honk!');
     return;
   }
+
+   //check if the user is on iOS Safari, if they are, we initiate the safari audio "hack"
+   const ua = window.navigator.userAgent;
+   const iOS = !!ua.match(/iPad/i) || !!ua.match(/iPhone/i);
+   const webkit = !!ua.match(/WebKit/i);
+   const iOSSafari = iOS && webkit && !ua.match(/CriOS/i);
 
   const audio = new Audio(
     'https://res.cloudinary.com/jlengstorf/video/upload/q_auto/v1569957993/honk-sound.mp3',
